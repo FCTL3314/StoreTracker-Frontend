0daf6a097b5784df7fa473c56d740053dba8a143
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2024-09-02 23:36:43 +0300

    Updates

---
+++ assets/NotFoundView-483e95ba.js
@@ -0,0 +1,2 @@
+import{E as e}from"./ErrorSection-1a8a4bfb.js";import{o as t,a as o}from"./index-1bf01cca.js";const c={__name:"NotFoundView",setup(r){return(a,n)=>(t(),o(e,{title:"404 Not Found",description:`We were unable to find the page you were interested in,\r
+                   it seems that it was deleted or did not exist at all.`}))}};export{c as default};
